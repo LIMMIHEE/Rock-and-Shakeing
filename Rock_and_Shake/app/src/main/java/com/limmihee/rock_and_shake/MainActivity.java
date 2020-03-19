e59bdfcf62413868487165c6596fbdbfbae0e52d
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-03-19 19:15:38 +0900

    기본 토대 완성

--- Rock_and_Shake/app/src/main/java/com/limmihee/rock_and_shake/MainActivity.java
+++ Rock_and_Shake/app/src/main/java/com/limmihee/rock_and_shake/MainActivity.java
@@ -3,6 +3,7 @@ package com.limmihee.rock_and_shake;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
@@ -10,9 +11,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    SensorManager manager =(SensorManager) getSystemService(Context.SENSOR_SERVICE);
-    Sensor M_sensor = manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-    SensorEventListener M_Event_L ;
+    private Accelometer accelometer;
+    private Gyroscop gyroscop;
 
     TextView MainV = (TextView)findViewById(R.id.MainText);
 
@@ -21,7 +21,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        accelometer = new Accelometer(this);
+        gyroscop= new Gyroscop(this);
+
+        accelometer.setListener(new Accelometer.Listener() {
+            @Override
+            public void onTransration(float tx, float ty, float tz) {
+            //    if(tz > 1.0f){
+            //        getWindow().getDecorView().setBackgroundColor(Color.RED);
+            //    }else if(tz < -1.0f){
+            //        getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+            //    }
+            }
+        });
+
+        gyroscop.setListener(new Gyroscop.Listener() {
+            @Override
+            public void onRotation(float rx, float ry, float rz) {
+                if(rz > 1.0f){
+
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+               }else if(rz < -1.0f){
+
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+            }
+        });
+
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        accelometer.Register();
+        gyroscop.Register();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        accelometer.unRegister();
+        gyroscop.unRegister();
+    }
 }
